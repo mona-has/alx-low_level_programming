d58eb6f2516e4ee16c3fe4210a682fdfbd1e1f52
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-10 13:58:11 -0700

    mm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main (void){
-    puts("\"programming is like building a multilingual puzzle");
-    return (0);	
-}	
+
+/**
+ * main - Entery point
+ *
+ * Description: A C program that print with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
