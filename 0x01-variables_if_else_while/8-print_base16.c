9c324ccd01b915f77f09ebec8d21c2649e3432d0
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-11 14:50:20 -0700

    mm

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Hexadecimal
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int d = '0';
+
+	while (d <= 'f')
+	{
+		putchar(d);
+		if (d == '9')
+			putchar(d += 39);
+		d++;
+	}
+		putchar('\n');
+	return (0);
+}
