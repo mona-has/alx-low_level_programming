9422b74f0dd77fa564bc04f590d0aa02884be06d
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 16:52:23 -0700

    mm

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - if n greater than 0 0r equll to 0
+ *       or lessthan 0
+ *
+ * @n: take intger type input for function
+ *
+ *Return: 1  if n is greater than zero
+ *        0  if n is zero
+ *        -1 if n is less than zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+
+	else if (n < 0)
+		_putchar('-');
+		return (-1);
+}
