9d8e754bf65c531dafbeff5ef29f43537068be6a
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 15:16:45 -0700

    mm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - print alphabet by _putchar function
  *
- * print_alphabet: print alphabet by _putchar function
- *
- * Return: Always 0.
- */
+*/
 
 void print_alphabet(void)
 {
