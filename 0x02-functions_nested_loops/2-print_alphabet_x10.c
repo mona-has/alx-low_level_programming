8350cb593a83f4872d87371478d876cfe5fb8ca9
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 15:37:02 -0700

    mm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int t = 1, x = 'a';
 
-	for (x = 'a'; x <= 'z'; x++)
+	for (t = 1; t <= 10; t++)
 	{
-		for (t = 1; t <= 10; t++)
+		for (x = 'a'; x <= 'z'; x++)
 			_putchar(x);
 	}
 	_putchar('\n');
