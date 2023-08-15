19e015423e2bdaf2a1dc484a74ba38cc0da06d6e
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 15:55:18 -0700

    mm

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - print lowercaseof alphabet
+ *
+ * @c: check input of function
+ *
+ * Return: 1 if c is lowercase
+ *         0 otherwise
+*/
+
+int _islower(int c)
+{
+	int x = 'a';
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		return (1);
+	}
+	return (0);
+}
