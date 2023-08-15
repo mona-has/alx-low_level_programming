2f4a727f3dbe90790c41d18fddffff75350d5992
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 14:38:20 -0700

    mm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+	return (0);
+}
