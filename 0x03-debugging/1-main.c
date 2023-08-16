696bfd2fb18c09c216cdc7bd454f0986ae8a9fef
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-16 10:54:38 -0700

    mm

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	} /** increment of while loop*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
