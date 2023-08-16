fddc1fe08beeab9ad1b5f821d167dd70bd68ea58
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 19:07:17 -0700

    mm

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,14 @@ void times_table(void)
 	for (x = 0; x <= 9; x++)
 	{
 		_putchar('0');
-		for (y = 0; y <= 9; y++)
+		for (y = 1; y <= 9; y++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			z = x * y;
 
-			if (z < 9)
+			if (z <= 9)
 				_putchar(' ');
 			else
 				_putchar((z / 10) + 48);
