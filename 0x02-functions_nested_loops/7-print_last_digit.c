32c3cf1b24446700913c33f1f32eb4704ecac4b9
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 17:35:17 -0700

    mm

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,9 @@ int print_last_digit(int n)
 	int ld;
 
 	if (n < 0)
-		n = (-1) * (n % 10);
+		ld = (-1) * (n % 10);
 	else
-		n = (n % 10);
+		ld = (n % 10);
 	_putchar(ld + '0');
 	return (ld);
 }
