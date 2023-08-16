8904d25958ed363b3f8de1aad49a8c51c80b08db
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-16 11:04:35 -0700

    mm

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);/** increment of while loop*/
-	}
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
