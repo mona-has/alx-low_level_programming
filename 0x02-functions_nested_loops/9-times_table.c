d27e2dff6503324e50657bd7a434c395ba3c9c75
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 19:01:53 -0700

    mm

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: always 0
+*/
+
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+		for (y = 0; y <= 9; y++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			z = x * y;
+
+			if (z < 9)
+				_putchar(' ');
+			else
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+			}
+		_putchar('\n');
+	}
+}
