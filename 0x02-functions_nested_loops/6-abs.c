ea0620c5834337343b43ba8c52c3725ac39bed12
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-15 17:15:24 -0700

    mm

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of an integer
+ *
+ * @n: take integer type for function
+ *
+ * Return: always 0
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	return (0);
+}
