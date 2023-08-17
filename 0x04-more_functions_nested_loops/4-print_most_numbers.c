fc36e416a89d063ea764a96ce28a589c1f640d40
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 12:30:07 -0700

    mm

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,8 +8,9 @@ void print_most_numbers(void)
 {
 	int a = '0';
 
-	if (a <= '9' && (a != '2' && a != '4'))
+	for (; a <= '9'; a++)
 	{
+		if (a != '2' || a != '4')
 		_putchar(a);
 	}
 	_putchar('\n');
