4423c70541fcee1cb195d9850b16cc71e308d553
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 14:50:00 -0700

    mm

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,14 @@
 
 void print_line(int n)
 {
+	int x;
 
-	for (n = 1; n < 0; n++)
+	for (x = 1; x <= n; x++)
 	{
 		_putchar('_');
 	_putchar('\n');
 	}
-	if (n == 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
