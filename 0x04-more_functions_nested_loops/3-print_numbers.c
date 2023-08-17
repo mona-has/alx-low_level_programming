6d73645f2a5dc7162404a128e1ccc017eea70477
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 10:43:32 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - numbers, from 0 to 9, followed by a new line
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
