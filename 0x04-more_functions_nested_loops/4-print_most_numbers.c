c04123ef746908576308c748bf2f904a460b6ff0
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 11:06:07 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Do not print 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	if (a <= '9' && (a != '2' || a != '4'))
+		_putchar(a);
+	_putchar('\n');
+}
