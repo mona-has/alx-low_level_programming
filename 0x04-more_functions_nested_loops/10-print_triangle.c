4c407d864cb90000c5c27e36c824ab9c49df1e8f
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 17:54:30 -0700

    mm

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,20 @@ void print_triangle(int size)
 {
 	int a, b;
 
-	for (a = 1; a <= size; a++)
-	{
-		for (b = 1; b <= size; b++)
-		{
-			if ((a + b) <= size)
-				_putchar(' ');
-			else
-				_putchar('#');
-		}
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if ((a + b) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
