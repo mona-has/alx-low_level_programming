8d3751ac2923f7ec71d86f1b4db56f2ce734f19d
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 17:14:02 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && !(a % 5 ==0))
+			printf("Fizz");
+		else if (a % 5 == 0 && !(a % 3 == 0))
+			printf("Buzz");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+
+		else
+			printf("%d", a);
+		if (a != 100)
+			printf(" ");
+		else
+		printf("\n");
+	}
+	return (0);
+}
