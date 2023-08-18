5bd32fec45ba6de0643e98261f5b26b7f99d7054
mooona <Mona.7assan.ahmed@gmail.com>
2023-08-17 19:53:18 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: input
+*/
+
+void print_number(int n)
+{
+	int m = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m;
+	}
+	if ((m / 10) > 0)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}
