b62a02773c93bc7e0ae3484064148e772aa371c2
mona <mona.7assan.ahmed@gmail.com>
2023-08-30 23:49:35 +0200

    mm

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - print string in reverse
+*
+* @s: stdin str
+*
+* Return: always 0
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s < '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s--);
+	}
+}
