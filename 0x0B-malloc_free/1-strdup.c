4e1d07d7682b244fc030c7969e3c1626e819dfa2
mona <mona.7assan.ahmed@gmail.com>
2023-09-05 23:23:38 +0200

    mm

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - duplicate string
+*
+* @str: stdin string
+*
+* Return: always 0
+*/
+
+char *_strdup(char *str)
+{
+	char *x;
+	int sz, y;
+
+	if (str == NULL)
+		return (NULL);
+	x = malloc(sz * sizeof(*x + 1));
+	for (sz = 0; sz != '\0'; sz++)
+
+		if (x == 0)
+			return (NULL);
+	for (y = 0; y < sz; y++)
+		x[y] = str[y];
+	return (x);
+}
