7f36704a4b6b101a6dfa0fb74b4f6500a3dbdca8
mona <mona.7assan.ahmed@gmail.com>
2023-09-05 23:25:18 +0200

    mm

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,9 +18,9 @@ char *_strdup(char *str)
 		return (NULL);
 	x = malloc(sz * sizeof(*x + 1));
 	for (sz = 0; sz != '\0'; sz++)
-
-		if (x == 0)
-			return (NULL);
+	;
+	if (x == 0)
+		return (NULL);
 	for (y = 0; y < sz; y++)
 		x[y] = str[y];
 	return (x);
