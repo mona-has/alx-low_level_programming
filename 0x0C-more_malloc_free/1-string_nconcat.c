d9c6b9cdfc249c128f5d9678be1c39cdb21b9889
mona <mona.7assan.ahmed@gmail.com>
2023-09-09 00:26:47 +0200

    mm

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *x;
 	unsigned int len1, len2, a, b;
+	int nn = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,8 +25,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	;
 	for (len2 = 0; len2 != '\0'; len2++)
 	;
-	if (n >= len2)
+	if (nn >= len2)
 	{
+		nn = n;
 		x = malloc(sizeof(char) * (len1 + len2 + 1));
 	}
 	else
