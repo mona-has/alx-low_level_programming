f6176c6458aec84d15a50316feb96893000e3c5e
mona <mona.7assan.ahmed@gmail.com>
2023-09-14 15:32:24 +0200

    mm

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+* init_dog - entry point
+*
+* @d: input
+* @name: input
+* @age: input
+* @owner: input
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		D->owner = owner;
+	}
+}
