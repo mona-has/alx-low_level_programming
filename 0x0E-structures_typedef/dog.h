556c9fc7127253b4b7e32ae8ded9619b284bc20f
mona <mona.7assan.ahmed@gmail.com>
2023-09-14 15:15:32 +0200

    mm

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-
+/**
+* struct dog - entry point
+*
+* @name: input
+* @age: input
+* @owner: input
+*/
 
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 
