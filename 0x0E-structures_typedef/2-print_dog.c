15092e8b940acb1a244406e0970229a6a25ac98c
mona <mona.7assan.ahmed@gmail.com>
2023-09-14 15:59:52 +0200

    mm

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - entry point
+*
+* @d: input
+*
+* Return: always 0
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+		printf("Age: %f\n", d->age);
+	}
+}
