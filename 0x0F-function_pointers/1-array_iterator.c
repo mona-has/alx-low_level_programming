b13833195d11a317eb75131edaa91e172f499ff8
mona <mona.7assan.ahmed@gmail.com>
2023-09-16 01:24:13 +0200

    mm

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - entry point
+*
+* @array: input
+* @size: array size
+* @action: pointer
+*
+* Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+* int_index - entry point
+*
+* @array: input
+* @size: input
+* @cmp: input
+*
+* Return: always 0
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+	for (x = 0; x < size. x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
