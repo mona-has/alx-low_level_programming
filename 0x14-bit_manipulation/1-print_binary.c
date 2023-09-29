14a28cc25d8b266820b879ab0ce2eb335e4ba0bd
Mooona <Mona.7assan.ahmed@gmail.com>
2023-09-29 04:55:33 +0200

    mm

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <math.h>
+
 
 /**
 * print_binary - entry point
@@ -11,15 +11,6 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		_putchar('0');
-		return;
-	else if (n == 1)
-	}
-		_putchar('1');
-		return;
-	}
-	print_binary(n >> 1);
-	_puutchar('0' + (n & 1));
+
 }
 
