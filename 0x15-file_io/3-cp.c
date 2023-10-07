6fc5b331cfc92f044ec2eadda6bd0def8beb1d5f
Mooona <Mona.7assan.ahmed@gmail.com>
2023-10-07 04:57:11 +0200

    mm

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,19 +12,17 @@
 int main(int argc, char **argv)
 {
 	int file_from, file_to;
-	int n1, n2;
+	int n1 = 1024, n2 = 0;
 	char d[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
@@ -33,23 +31,21 @@ int main(int argc, char **argv)
 		close(file_from);
 		exit(99);
 	}
-
-	n1 = 1024;
-	n2 = 0;
 	while (n1 == 1024)
 	{
 		n1 = read(file_from, d, 1024);
 		if (n1 == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
 		n2 = write(file_to, d, n1);
 		if (n2 < n1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
-
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file_from), exit(100);
 	if (close(file_to) == -1)
