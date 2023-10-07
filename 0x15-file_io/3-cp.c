08e98f8a434097be6fbe5d4070180e5092cbc350
Mooona <Mona.7assan.ahmed@gmail.com>
2023-10-07 04:48:03 +0200

    mm

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+* main - entry point
+*
+* @argc: numbers of arguments
+* @argv: string
+*
+* Return: always 0
+*/
+
+int main(int argc, char **argv)
+{
+	int file_from, file_to;
+	int n1, n2;
+	char d[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+		S_IRUSER | S_IWUSER | S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+
+	n1 = 1024;
+	n2 = 0;
+	while (n1 == 1024)
+	{
+		n1 = read(file_from, d, 1024);
+		if (n1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", argv[1]), exit(98);
+		}
+		n2 = write(file_to, d, n1);
+		if (n2 < n1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]), exit(99);
+		}
+	}
+
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", file_to), exit(100);
+	return (0);
+}
