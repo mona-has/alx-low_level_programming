5ae2593a712d01eceb45545017e1cf5819a118ae
Mooona <Mona.7assan.ahmed@gmail.com>
2023-10-27 02:00:31 +0200

    mm

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,4 +11,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
+	unsigned long int q;
+	unsigned long int u = 0;
+
+	q = n ^ m;
+	while (q > 0)
+	{
+		u += q & 1;
+		q >>= 1;
+	}
+	return (u);
+}
