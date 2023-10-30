56af9df63ab5c909976fddf788ffb5a072c7f6af
Mooona <Mona.7assan.ahmed@gmail.com>
2023-10-30 22:01:39 +0200

    mm

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* read_textfile - entry point
+*
+* @filename: file to be read
+* @letters: bytes
+*
+* Return: actual number of letters
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fi;
+	ssize_t lee;
+	char *b;
+
+	if (filename == NULL || letters == NULL)
+		return (0);
+	b == malloc(sizeof(char) * (letters));
+	if (b == NULL)
+		return (0);
+
+	fi = open(filename, O_RDONLY);
+	if (fi == -1)
+		return (0);
+	lee = read(fi, b, letters);
+	lee = write(STDOUT_FILENO, b, lee);
+
+	free(b);
+	close(fi);
+	return (lee);
+}
