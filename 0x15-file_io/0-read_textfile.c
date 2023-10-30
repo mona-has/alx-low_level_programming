907d2cfef494fee0fa7f46b1d2aa171707fbed80
Mooona <Mona.7assan.ahmed@gmail.com>
2023-10-30 22:09:02 +0200

    mm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,9 +15,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t lee;
 	char *b;
 
-	if (filename == NULL || letters == NULL)
+	if (!filename || !letters)
 		return (0);
-	b == malloc(sizeof(char) * (letters));
+	b = malloc(sizeof(char) * (letters));
 	if (b == NULL)
 		return (0);
 
