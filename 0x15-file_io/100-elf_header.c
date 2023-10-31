c0c68b091fd0f3b79733689440cc517c973323c3
Mooona <Mona.7assan.ahmed@gmail.com>
2023-10-31 02:29:15 +0200

    mm

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _moo_on - entry point
+*
+* Return: always 0
+*/
+
+int _moo_on(void)
+{
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUF_SIZE 1024
-
 #include <stddef.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -14,5 +12,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char **argv);
+int _moo_on(void);
 
 #endif
